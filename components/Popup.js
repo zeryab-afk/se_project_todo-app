e60e931bbe2133395e7c6655c9abc25a442f318f
zeryab-afk <zaryabshafiq54@gmail.com>
2025-07-29 16:51:48 +0500

    Add files via upload

---
+++ components/Popup.js
@@ -0,0 +1,33 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._handleEscapeClose = this._handleEscapeClose.bind(this);
+  }
+
+  open() {
+    this._popup.classList.add('popup_visible');
+    document.addEventListener('keydown', this._handleEscapeClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_visible');
+    document.removeEventListener('keydown', this._handleEscapeClose);
+  }
+
+  _handleEscapeClose(evt) {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.addEventListener('mousedown', (evt) => {
+      if (evt.target.classList.contains('popup_visible')) { 
+        this.close();
+      }
+      if (evt.target.classList.contains('popup__close')) {
+        this.close();
+      }
+    });
+  }
+}
---
+++ components/TodoCounter.js
@@ -0,0 +1,22 @@
+export default class TodoCounter {
+  constructor(todos, selector) {
+    this._element = document.querySelector(selector);
+    this._completed = todos.filter(todo => todo.completed).length;
+    this._total = todos.length;
+    this._updateText();
+  }
+
+  updateCompleted(increment) {
+    this._completed += increment ? 1 : -1;
+    this._updateText();
+  }
+
+  updateTotal(increment) {
+    this._total += increment ? 1 : -1;
+    this._updateText();
+  }
+
+  _updateText() {
+    this._element.textContent = `Showing ${this._completed} out of ${this._total} completed`;
+  }
+}
