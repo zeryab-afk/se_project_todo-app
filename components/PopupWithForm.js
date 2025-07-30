88920335afaaf15838c767f5e30810021abc3c6f
zeryab-afk <zaryabshafiq54@gmail.com>
2025-07-30 11:23:06 +0500

    PopupWithForm.js

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -25,8 +25,5 @@ export default class PopupWithForm extends Popup {
     });
   }
 
-  close() {
-    super.close();
-    this._form.reset();
-  }
+  // <<< Removed close() override since parent method is sufficient
 }
